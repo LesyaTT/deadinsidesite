a529a1419ed4bc56dbcbf112c1a8e9f091368c38
Олеся Невская <savinen@mail.ru>
2022-09-29 21:15:32 +0500

    smth

--- js/test.js
+++ js/test.js
@@ -1,5 +1,5 @@
 class test {
-    constructor (questions, results) {
+    constructor(questions, results) {
         this.questions = questions;
         this.results = results;
 
@@ -18,7 +18,7 @@ class test {
             correctAnsw = index;
         } else {
             for (let i = 0; i < this.questions[this.currentQuest].answers.lenght; i++) {
-                if(this.questions[this.currentQuest].answers[i].value >= 1) {
+                if (this.questions[this.currentQuest].answers[i].value >= 1) {
                     correctAnsw = i;
                     break;
                 }
@@ -38,160 +38,198 @@ class test {
     }
 
     End() {
-        for(let i = 0; i < this.results.lenght; i++) {
-            if(this.results[i].Check(this.score)) {
+        for (let i = 0; i < this.results.lenght; i++) {
+            if (this.results[i].Check(this.score)) {
                 this.result = i;
             }
         }
     }
 }
 
-class Question
-{
-   constructor(text, answers)
-   {
-       this.text = text;
-       this.answers = answers;
-   }
- 
-   Click(index)
-   {
-       return this.answers[index].value;
-   }
+class Question {
+    constructor(text, answers) {
+        this.text = text;
+        this.answers = answers;
+    }
+
+    Click(index) {
+        return this.answers[index].value;
+    }
 }
 
-class Answer
-{
-   constructor(text, value)
-   {
-       this.text = text;
-       this.value = value;
-   }
+class Answer {
+    constructor(text, value) {
+        this.text = text;
+        this.value = value;
+    }
 }
 
-class Result
-{
-   constructor(text, value)
-   {
-       this.text = text;
-       this.value = value;
-   }
- 
-   //Этот метод проверяет, достаточно ли очков набрал пользователь
-   Check(value)
-   {
-       if(this.value <= value)
-       {
-           return true;
-       }
-       else
-       {
-           return false;
-       }
-   }
+class Result {
+    constructor(text, value) {
+        this.text = text;
+        this.value = value;
+    }
+
+    //Этот метод проверяет, достаточно ли очков набрал пользователь
+    Check(value) {
+        if (this.value <= value) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
 
 const results =
-[
-   new Result("Вы явно не дед инсайд, вам до этого еще расти и расти.", 0),
-   new Result("Вы стараетесь, но если честно, у вас не очень хорошо выходит.", 4),
-   new Result("Вы на верном пути становления, продолжайте.", 8),
-   new Result("Вы явно дед инсайд, советую посетить психолога, а лучше сразу психиатра.", 12),
-   new Result("Да вы самый дед инсайдный дед инсайд из всех дед инсайдов мира, готова оплатить психиатра.", 16)
-];
- 
+    [
+        new Result("Вы явно не дед инсайд, вам до этого еще расти и расти.", 0),
+        new Result("Вы стараетесь, но если честно, у вас не очень хорошо выходит.", 4),
+        new Result("Вы на верном пути становления, продолжайте.", 8),
+        new Result("Вы явно дед инсайд, советую посетить психолога, а лучше сразу психиатра.", 12),
+        new Result("Да вы самый дед инсайдный дед инсайд из всех дед инсайдов мира, готова оплатить психиатра.", 16)
+    ];
+
 const questions =
-[
-   new Question("Играете ли вы в доту?",
-   [
-       new Answer("Да", 1),
-       new Answer("Нет", 0)
-   ]),
+    [
+        new Question("Играете ли вы в доту?",
+            [
+                new Answer("Да", 1),
+                new Answer("Нет", 0)
+            ]),
 
-   new Question("Ваш любимый персонаж Shadow Fiend?",
-   [
-       new Answer("Да", 1),
-       new Answer("Нет", 0)
-   ]),
+        new Question("Ваш любимый персонаж Shadow Fiend?",
+            [
+                new Answer("Да", 1),
+                new Answer("Нет", 0)
+            ]),
 
-   new Question("У вас осветлены волосы?",
-   [
-       new Answer("Да", 1),
-       new Answer("Нет", 0)
-   ]),
-   
-   new Question("Вы нервный человек?",
-   [
-       new Answer("Да", 1),
-       new Answer("Нет", 0)
-   ]),
+        new Question("У вас осветлены волосы?",
+            [
+                new Answer("Да", 1),
+                new Answer("Нет", 0)
+            ]),
 
-   new Question("Знаете ли вы кто такой Папич?",
-   [
-       new Answer("Да", 1),
-       new Answer("Нет", 0)
-   ]),
+        new Question("Вы нервный человек?",
+            [
+                new Answer("Да", 1),
+                new Answer("Нет", 0)
+            ]),
 
-   new Question("Часто ли вы используете ненормативную лексику?",
-   [
-       new Answer("Да", 1),
-       new Answer("Нет", 0)
-   ]),
+        new Question("Знаете ли вы кто такой Папич?",
+            [
+                new Answer("Да", 1),
+                new Answer("Нет", 0)
+            ]),
 
-   new Question("Друзья/родственники/знакомые считают вас психом?",
-   [
-       new Answer("Да", 1),
-       new Answer("Нет", 0)
-   ]),
+        new Question("Часто ли вы используете ненормативную лексику?",
+            [
+                new Answer("Да", 1),
+                new Answer("Нет", 0)
+            ]),
 
-   new Question("Вы сами считаете себя конченным?",
-   [
-       new Answer("Да", 0),
-       new Answer("Нет", 1)
-   ]),
+        new Question("Друзья/родственники/знакомые считают вас психом?",
+            [
+                new Answer("Да", 1),
+                new Answer("Нет", 0)
+            ]),
 
-   new Question("Вы смотрите аниме?",
-   [
-       new Answer("Да", 1),
-       new Answer("Нет", 0)
-   ]),
+        new Question("Вы сами считаете себя конченным?",
+            [
+                new Answer("Да", 0),
+                new Answer("Нет", 1)
+            ]),
 
-   new Question("Одно из ваших любимых аниме 'Токийский Гуль'?",
-   [
-       new Answer("Да", 1),
-       new Answer("Нет", 0)
-   ]),
+        new Question("Вы смотрите аниме?",
+            [
+                new Answer("Да", 1),
+                new Answer("Нет", 0)
+            ]),
 
-   new Question("Знаете ли вы наизусть опенинг 'Токийского Гуля'?",
-   [
-       new Answer("Да", 1),
-       new Answer("Нет", 0)
-   ]),
+        new Question("Одно из ваших любимых аниме 'Токийский Гуль'?",
+            [
+                new Answer("Да", 1),
+                new Answer("Нет", 0)
+            ]),
 
-   new Question("Знаете ли вы сколько будет 1000-7?",
-   [
-       new Answer("Да", 1),
-       new Answer("Нет", 0)
-   ]),
+        new Question("Знаете ли вы наизусть опенинг 'Токийского Гуля'?",
+            [
+                new Answer("Да", 1),
+                new Answer("Нет", 0)
+            ]),
 
-   new Question("Преимущественно ваш гардероб чёрного цвета?",
-   [
-       new Answer("Да", 1),
-       new Answer("Нет", 0)
-   ]),
+        new Question("Знаете ли вы сколько будет 1000-7?",
+            [
+                new Answer("Да", 1),
+                new Answer("Нет", 0)
+            ]),
 
-   new Question("Вы водили чью-то маму в кино?",
-   [
-       new Answer("Да", 1),
-       new Answer("Нет", 0)
-   ]),
+        new Question("Преимущественно ваш гардероб чёрного цвета?",
+            [
+                new Answer("Да", 1),
+                new Answer("Нет", 0)
+            ]),
 
-   new Question("Зависимы ли вы от никотина или алкоголя?",
-   [
-       new Answer("Да", 1),
-       new Answer("Нет", 0)
-   ])
-];
- 
+        new Question("Вы водили чью-то маму в кино?",
+            [
+                new Answer("Да", 1),
+                new Answer("Нет", 0)
+            ]),
+
+        new Question("Зависимы ли вы от никотина или алкоголя?",
+            [
+                new Answer("Да", 1),
+                new Answer("Нет", 0)
+            ])
+    ];
+
 const test = new test(questions, results);
 
+
+Update();
+
+function Update() {
+    if (test.currentQuest < test.questions.lenght) {
+        headElement.innerHTML = test.questions[test.currentQuest].text;
+
+
+        for (let i = 0; i < test.questions[test.currentQuest].answers.length; i++) {
+            let btn = document.createElement("button");
+            btn.className = "btn";
+
+            btn.innerHTML = test.questions[test.currentQuest].answers[i].text;
+
+            btn.setAttribute("index", i);
+
+            buttonsElem.appendChild(btn);
+
+            Init();
+        }
+    } else {
+        buttonsElem.innerHTML = "";
+        headElem.innerHTML = test.results[test.result].text;
+        pagesElem.innerHTML = "Баллов: " + test.score;
+    }
+}
+
+function Init() {
+    let btns = document.getElementsByClassName("btn");
+
+    for (let i = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", function (e) { Click(e.target.getAttribute("index")); });
+    }
+}
+
+function Click(index) {
+    let correct = test.Click(index);
+
+    let btns = document.getElementsByClassName("btn");
+
+    for (let i = 0; i < btns.length; i++) {
+        btns[i].className = "btn btn_passive";
+    }
+
+    btns[index].className = "btn btn_correct";
+     
+    setTimeout(Update, 1000);
+}
